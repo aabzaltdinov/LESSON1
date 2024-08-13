a5d5ca3f5ea5c117c0607f485fd3987591384ad1
aabzaltdinov <aabzaltdinov@gmail.com>
2024-08-13 22:10:39 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+name = 'python'
+home_works_hours = 12
+hours_spent = 1.5
+average_task_completed = hours_spent/home_works_hours
+print('курс',':',name,',','всего задач',':',home_works_hours,',','затрачено часов',':',hours_spent,',','среднее время выполнения',average_task_completed,'часа','.')
