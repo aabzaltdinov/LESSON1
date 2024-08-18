122ecc19eff7814e12e584f96f389a375b9ce37d
aabzaltdinov <aabzaltdinov@gmail.com>
2024-08-18 18:36:52 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var=1,True,'apple',4
+print(immutable_var)
+immutable_var=1,3,False,'orange'
+print(immutable_var)
+mutable_list=(['apple','orange','carrot','potato'])
+print(mutable_list)
+mutable_list[0]='cucumber'
+print(mutable_list)
