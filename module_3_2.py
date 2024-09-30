295088584116d5698163b05c3a84a698bae751d0
aabzaltdinov <aabzaltdinov@gmail.com>
2024-09-30 21:59:20 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,12 +1,16 @@
 def send_email(message,recipient,sender = "university.help@gmail.com"):
-    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    elif recipient == sender:
-        print("Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com" :
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    elif sender != "university.help@gmail.com" :
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+    full_name_recipient = '@' in recipient and (recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net'))
+    full_name_sender = '@' in sender and (sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net'))
+    if full_name_recipient and full_name_sender:
+        if recipient == sender:
+            print('Нельзя отправить письмо самому себе!')
+        else:
+            if sender == 'university.help@gmail.com':
+                print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+            else:
+                print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+    else:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
